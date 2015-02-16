234690fc9ea183b6100294bd9f6914c0ce784105
Vitezslav Benes <vitezslav.benes@gmail.com>
2015-02-16 13:29:37 +0100

    Started to develop the actual mechanism

--- js/learn.js
+++ js/learn.js
@@ -1,34 +1,81 @@
-document.addEventListener( "DOMContentLoaded", function() {
-	document.onkeydown = function (evt) {
+document.addEventListener( "DOMContentLoaded", init);
+
+function init() {
+	/*document.onkeydown = function (evt) {
 		var keyCode = evt ? (evt.which ? evt.which : evt.keyCode) : event.keyCode;
 		if (keyCode == 13) {
-			savePair();
+			addPair();
 		}
-	}
-
-
-});
-
-var pairArray = [];
-
-function Pair(key, value) {
-	this.key = key;
-	this.value = value;
-	this.toString = function() {
-		return this.key + " : " + this.value;
-	}
+	}*/
 }
 
-function savePair() {
+var pairDatabase = [["Lorem", "Ipsum"], ["Dog", "Woof"], ["Cat", "Miau"], ["Bacon", "Fat"], ["Jon", "Snow"], ["Tywin", "Lannister"], ["Tyrion", "Lannister"]];
+
+var minPairsGenerated = 5;
+
+function generatePairs() {
+	var numberOfPairs = parseInt(document.getElementsByTagName("input")[name="num-of-pairs"].value);
+
+	var pairsChosen = [];
+
+	while (numberOfPairs !== (pairsChosen.length)) {
+		var randomNumber = Math.floor((Math.random() * pairDatabase.length));
+
+		if (pairsChosen.indexOf(randomNumber) === -1) {
+			pairsChosen.push(randomNumber);
+		}
+	}
+	console.log(pairsChosen);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+	TRIED IT ANYWAY
+	function Pair(key, values) {
+	this.key = key;
+	this.values = values || [];
+	this.position = 0;
+
+	for (i = 0; i < values.length; i++) {
+		this.values[i] = values[i];
+	}
+
+	this.toString = function() {
+		return this.key + " : " + this.values.toString();
+	}
+	this.save = function() {
+		this.position = pairArray.length;
+		pairArray[pairArray.length] = this;
+		// HIDEOUS !!!!!! localStorage.pairArray += "," + pairArray; BUT WORKS
+
+	}
+}*/
+
+/*function addPair() {
 	var inputs = document.getElementsByTagName("input");
 
-	var pair = new Pair(inputs[0].value, inputs[1].value);
+	// var pair = new Pair(inputs[0].value, inputs[1].value);
+	// pair.save();
 
+	var pair = [];
+	pair[inputs[0].value] = inputs[1].value;
 	pairArray[pairArray.length] = pair;
 
-	localStorage.pairArray += "," + pairArray;
-
-	addToPairList(pair);
+	console.log(pair);
+	addRow(pair);
 }
 
 function showPairs() {
@@ -37,22 +84,24 @@ function showPairs() {
 	}
 }
 
-function addToPairList(pair) {
+function deleteRow(button) {
+	var row=button.parentNode.parentNode;
+	row.parentNode.removeChild(row);
+}
+
+function addRow(pair) {
 	var table = document.getElementById("pair-table");
 	var row = table.insertRow(0);
 	var cell1 = row.insertCell(0);
 	var cell2 = row.insertCell(1);
 	var cell3 = row.insertCell(2);
 
-	cell1.innerHTML = pair.key;
-	cell2.innerHTML = pair.value;
+	for(var index in pair) {
+	cell1.innerHTML = index;
+	cell2.innerHTML = pair.index;
 	cell3.innerHTML = "<button class='del-row-button' onclick='deleteRow(this)'>delete</button>"
-}
-
-function deleteRow(button) {
-	var row=button.parentNode.parentNode;
-	row.parentNode.removeChild(row);
-}
+	}
+}*/
 
 /*window.onbeforeunload = function(e) {
     return "Sure you want to leave?";
--- learn.php
+++ learn.php
@@ -8,15 +8,33 @@
 <body id="learn">
     <?php if (isset($_SERVER["HTTP_REFERER"])) echo "<a href='" . $_SERVER["HTTP_REFERER"] . "' id='backbutton'>back</a>" ?>
     <main>
-    	<div class="add-pair">
+    	<!--
+    	okay punk, that's adding a pair, not that it works anyway
+    	let's now work with the assumption that I do have some pairs already defined and need to program the actual mechanism
+    	-->
+    	<div class="controls">
     		<input type="text" name="pairKey" value="tyrion" autocomplete="off">
     		<input type="text" name="pairValue" value="lannister"  autocomplete="off">
-    		<button onclick="savePair()">save</button>
+    		<button onclick="addPair()">save</button>
+
+    		<!-- <label for="num-of-pairs">enter number of pairs for practise</label> -->
+    		<input type="number" name="num-of-pairs" value="6" placeholder="5">
+    		<button onclick="generatePairs()">set</button>
     	</div>
     	<div class="pair-list">
-    		<table id="pair-table">
-    		</table>
+    		<table id="pair-table"></table>
+
+    		<div class="leftcol">
+    			<ul>
+    			</ul>
+    		</div>
+
+    		<div class="rightcol">
+    			<ul>
+    			</ul>
+    		</div>
     	</div>
+
 	</main>
 </body>
 </html>
