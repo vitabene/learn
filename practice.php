d57774ae5eab27d7ccc0e4b1520eb527c9e147bc
Vitezslav Benes <vitezslav.benes@gmail.com>
2015-03-29 12:54:05 +0200

    Created a page for practise, modified styles

---
+++ practice.php
@@ -0,0 +1,40 @@
+<?php
+require './includes/init.php';
+
+if (isset($_GET['id']) && Db::query("SELECT * FROM set_names WHERE id=?", $_GET['id'])) {
+
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Learn - home</title>
+        <?php require 'includes/head.php'; ?>
+
+    </head>
+    <body id="learn">
+        <nav>
+            <?php require 'includes/nav.php'; ?>
+        </nav>
+
+        <main class="content">
+            <div class="heading">
+            <h2>press start when you are ready</h2>
+            </div>
+            <div id="pair-list">
+                <ul id="leftcol"></ul>
+                <ul id="rightcol"></ul>
+            </div>
+            <div class="controls">
+                <button id="startbutton" <?php echo "data-set='" . urlencode($_GET['id']) . "' "; ?>>start</button>
+                <button id="checkbutton">check</button>
+            </div>
+
+        </main>
+
+        <script src="./js/build/application.min.js"></script>
+    </body>
+    </html>
+    <?php } else {
+        header('Location: index.php');
+    }
+    ?>
