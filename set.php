a17780d87a1914634690a000e91184e4efc9f5ee
Vitezslav Benes <vitezslav.benes@gmail.com>
2015-04-03 21:12:47 +0200

    php for adding a key done, javascript to be done

--- set.php
+++ set.php
@@ -3,13 +3,46 @@ require './includes/init.php';
 
 if (!empty($_POST)) {
     //adding new key with value
-    var_dump($_POST);
     if (isset($_POST['key_name']) && isset($_POST['values_for_key']) && isset($_POST['set_id'])) {
         $table_name = 'key_to_values';
-        $values = explode(";", $_POST['values']);
-        $values = array_map('trim', $values);
-        $data = array('set_id' => $_POST['set_id'], 'key' => $_POST['key'], 'values' => json_encode($values));
-        Db::insert($table_name, $data);
+        $set_id = $_POST['set_id'];
+        $key = $_POST['key_name'];
+        $values = array_map('trim', explode(";", $_POST['values_for_key']));
+
+        $existing_pairs = Db::queryAll("SELECT * FROM " . $table_name . " WHERE set_id=?", $set_id);
+
+        $keys = array();
+        for ($i = 0; $i < sizeof($existing_pairs); $i++) {
+            $keys[$i] = $existing_pairs[$i]["key"];
+        }
+
+        $key_index = array_search($key, $keys);
+
+        if ($key_index === FALSE) {
+            $data = array('set_id' => $set_id, 'key' => $key, 'values' => $values);
+            Db::insert($table_name, $data);
+        } else {
+            //get row
+            $row = $existing_pairs[$key_index];
+            //get existing values
+            $existing_values = json_decode($row['values']);
+            //the value already exists, return
+            $original_size = sizeof($values);
+            for ($i = 0; $i < $original_size; $i++) {
+                if (in_array($values[$i], $existing_values, true)) {
+                    unset($values[$i]);
+                }
+            }
+            if (sizeof($values) !== 0) {
+                //join to new values
+                $data = array('values' => json_encode(array_merge($values, $existing_values)));
+                //update
+                $where_condition = "WHERE id=". $row['id'];
+                Db::update($table_name, $data, $where_condition);
+            } else {
+                $error = 'All values for selected key already exist';
+            }
+        }
     }
     //adding new set
     elseif (isset($_POST['new_set']) && !empty($_POST['new_set'])) {
@@ -94,17 +127,12 @@ if (!empty($_POST)) {
                 <td class="value-td">
                 </td>
             </tr></tbody></table>';
-            /*echo '<tr>
-                <td class="key-td">
-                    <a id="addKey">
-                        <span class="plus-key"></span>
-                    </a>
-                    <input type="text" name="key_name">
-                </td>
-                <td class="value-td">
-                <input type="text" name="values_for_key">
-                <input type="submit" value="add"></td>
-            </tr></tbody>';*/
+
+            if (!empty($error)) {
+                $error = "";
+                //different styling needed
+                echo "<div class='heading'><h4>" . $error . "</h4></div>";
+            }
         }
         //view set end
 
